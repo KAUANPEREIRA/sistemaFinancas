f1a7ad7cf89e85818abdbd8d6e13f4237d259536
KAUANSANTOS <66709503+KAUANPEREIRA@users.noreply.github.com>
2023-04-04 18:45:06 -0300

    sistema financas

---
+++ sistemafinancas/src/App.tsx
@@ -0,0 +1,19 @@
+import * as C from "./App.styles";
+const App = () => {
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        {/* area de informacoes resta 145 min */}
+
+        {/* area de inserção */}
+
+        {/* tabela de itens */}
+      </C.Body>
+    </C.Container>
+  );
+};
+
+export default App;
