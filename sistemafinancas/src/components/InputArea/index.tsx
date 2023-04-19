e9dd812301def46e4280dd953427ae660b0b171e
KAUANSANTOS <66709503+KAUANPEREIRA@users.noreply.github.com>
2023-04-19 13:02:04 -0300

    criacao do formulario

--- sistemafinancas/src/components/InputArea/index.tsx
+++ sistemafinancas/src/components/InputArea/index.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import { Item } from "../../types/Item";
 import * as C from "./styles";
 type Props = {
   onAdd: (item: Item) => void;
 };
 export const InputArea = ({ onAdd }: Props) => {
-  const handleAddEvent = () => {};
+  const [category, setCategory] = useState("");
+  const handleAddEvent = () => {
+    let newItem: Item = {
+      date: new Date(2023, 3, 19),
+      category: "food",
+      title: "item de teste",
+      value: 20.24,
+    };
+    onAdd(newItem);
+  };
   return (
     <C.Container>
+      <input></input>
       <button onClick={handleAddEvent}>Adicionar</button>
     </C.Container>
   );
